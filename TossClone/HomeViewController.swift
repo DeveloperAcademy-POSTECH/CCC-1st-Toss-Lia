82b80b0f1dd89b5f0df7e109a6b483fbc1535759
Lia316 <kjk0825@postech.ac.kr>
2022-07-03 00:29:13 +0900

    feat✨: set navigation items

--- TossClone/HomeViewController.swift
+++ TossClone/HomeViewController.swift
@@ -7,10 +7,59 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
+
+    private let logoView: UIStackView = {
+        let logoImage = UIImage(named: ImageLiteral.logo)
+        let button = UIButton()
+        button.setImage(logoImage, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        let spacer = UIView()
+        let stack = UIStackView(arrangedSubviews: [button, spacer])
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        return stack
+    }()
+
+    private let chatBarItem: UIBarButtonItem = {
+        let chatImage = UIImage(systemName: "message.fill")
+        let chatBarItem = UIBarButtonItem(image: chatImage, style: .plain,
+                                          target: HomeViewController.self, action: #selector(chatButtonPressed))
+        chatBarItem.tintColor = .systemGray
+        return chatBarItem
+    }()
+
+    private let bellBarItem: UIBarButtonItem = {
+        let bellImage = UIImage(systemName: "bell.fill")
+        let bellBarItem = UIBarButtonItem(image: bellImage, style: .plain,
+                                          target: HomeViewController.self, action: #selector(bellButtonPressed))
+        bellBarItem.tintColor = .systemGray
+        return bellBarItem
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationItem()
+        view.backgroundColor = .systemBackground
     }
 
+}
+
+extension HomeViewController {
+
+    private func setNavigationItem() {
+        navigationItem.titleView = logoView
+        navigationItem.rightBarButtonItems = [bellBarItem, chatBarItem]
+    }
+
+    @objc func chatButtonPressed() {
+    }
+
+    @objc func bellButtonPressed() {
+    }
+
+}
+
+enum ImageLiteral {
+    static let logo = "toss"
 }
