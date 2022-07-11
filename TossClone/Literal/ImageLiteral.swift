d0fb3c9531fc86c12a191c8086312c1ff9ba26d1
Lia316 <kjk0825@postech.ac.kr>
2022-07-11 23:09:38 +0900

    chore🧹: add bank images & fill image literal

--- TossClone/Literal/ImageLiteral.swift
+++ TossClone/Literal/ImageLiteral.swift
@@ -9,4 +9,14 @@ import Foundation
 
 enum ImageLiteral {
     static let logo = "toss"
+    static let bankIcon = "뱅크아이콘"
+    static let kwangjuBank = "광주은행"
+    static let shinhanBank = "신한은행"
+    static let kakaoBank = "카카오뱅크"
+    static let wooriBank = "우리은행"
+    static let nhBank = "NH농협은행"
+    static let hanaBank = "하나은행"
+    static let bnkBank = "BNK경남은행"
+    static let scBank = "SC제일은행"
+    static let okBank = "OK저축은행"
 }
